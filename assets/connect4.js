04dfcf908bc8551ce3a5edd437fa518a2307c4a3
DESKTOP-CAQ9A79\JYUN3 <jacob.yun429@gmail.com>
2018-04-28 15:41:12 -0700

    added basic functionality when hovering over board to locate next player move

--- assets/connect4.js
+++ assets/connect4.js
@@ -4,19 +4,49 @@ class Connect4 {
         this.cols = 7;
         this.selector = selector;
         this.createGrid();
+        this.setupEventListeners();
     }
 
     createGrid() {
-        const $board = $(this.selector);
+      const $board = $(this.selector);
         for (let row = 0; row < this.rows; row++) {
           const $row = $('<div>')
             .addClass('row');
           for (let col = 0; col < this.cols; col++) {
             const $col = $('<div>')
-            .addClass('col empty');
+            .addClass('col empty')
+            .attr('data-col', col)
+            .attr('data-row', row);
             $row.append($col);
           }
           $board.append($row);
         }
+    }
+
+    setupEventListeners() {
+      const $board = $(this.selector);
+
+      function findLastEmptyCell(col) {
+        const cells = $(`.col[data-col='${col}']`)
+        for (let i = cells.length - 1; i >= 0; i--) {
+            const $cell = $(cells[i]);
+            if ($cell.hasClass('empty')) {
+                return $cell;
+            }
+        }
+        return null;
+        console.log(cells);
+      }
+
+      $board.on('mouseenter', '.col.empty', function(){
+          const col = $(this).data('col');
+          const $lastEmptyCell = findLastEmptyCell(col);
+          $lastEmptyCell.addClass('next-drop');
+
+      })
+
+      $board.on('mouseleave', '.col', function() {
+          $('.col').removeClass('next-drop');
+      })
     }
 }
